14e1325b97f3aefc45c62aa3707801533dfaa3d0
smartymoon <pp787qq@gmail.com>
2021-10-08 16:03:59 +0800

    init inertia vue2 tailwindcss laravel-admin

---
+++ readme.md
@@ -0,0 +1,5 @@
+1. build environment
+2. install laravel-admin
+3. install inertia, enlighten, tailwindcss
+4. install and study passport
+5. study websocket, heroKu 
---
+++ resources/assets/js/Pages/Home/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-blue-200">
+    hello Vue3 index
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index"
+}
+</script>
+
+<style scoped>
+
+</style>
