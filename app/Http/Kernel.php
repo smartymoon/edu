333932fab3522d6faa82a57d0c3b26d9baf97e24
smartymoon <pp787qq@gmail.com>
2021-10-11 19:54:40 +0800

    use cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Http;
 
 use App\Http\Middleware\StudentAuthMiddleware;
 use App\Http\Middleware\TeacherAuthMiddleware;
+use Fruitcake\Cors\HandleCors;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -16,6 +17,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
